b74452e1c2f482aabd49b444d34f775f392ac980
Kadir <Kadir_yazici28@hotmail.de>
2022-12-08 23:07:57 +0100

    Menüseite

--- Bachelor/app.js
+++ Bachelor/app.js
@@ -1,15 +1,24 @@
-let searchBox = document.querySelector('#search-box');
+//let searchBox = document.querySelector('#search-box');
+//<input type="text" placeholder="search images" id="search-box">
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
 let images = document.querySelectorAll('.container .image-container .image');
 
-searchBox.oninput = () =>{
+const expand = () => {
+    searchBtn.classList.toggle("close");
+    input.classList.toggle("square");
+  };
+  searchBtn.addEventListener("click", expand);
+
+input.oninput = () =>{
    images.forEach(hide => hide.style.display = 'none');
-   let value = searchBox.value;
+   let value = input.value;
    images.forEach(filter =>{
       let title = filter.getAttribute('data-title');
       if(value == title){
          filter.style.display = 'block';
       }
-      if(searchBox.value == ''){
+      if(input.value == ''){
          filter.style.display = 'block';
       }
    });
