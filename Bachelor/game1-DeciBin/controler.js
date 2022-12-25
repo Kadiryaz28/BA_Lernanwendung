e6be646d6e2ecc8bc04174ad5039c452ceea3dfb
Kadir <Kadir_yazici28@hotmail.de>
2022-12-25 22:45:52 +0100

    Spiel ausgebaut

--- Bachelor/game1-DeciBin/controler.js
+++ Bachelor/game1-DeciBin/controler.js
@@ -3,30 +3,47 @@ let input1 = document.getElementById('input');
 let userInput = document.getElementById('userInput');
 let score = document.getElementById('score');
 let user_score = 0;
-let count_down = 10000;
+let count_down = 150;
 let count_down_element = document.getElementById("countDown");
 count_down_element.innerHTML = count_down;
 let inputted = false;
 
+
+//neue Elemente
+let digitStart = document.getElementById('digit1')
+
 function randomNum(){
-    return Math.ceil(Math.random() * 100);
+    return Math.ceil(Math.random() * 31);
 }
 randomNum();
+
 
 function display(){
     let number = randomNum();
     input1.value = number;
+    digitStart.value = input1.value;
 }
-
 display(); 
 
+function decitobin(wert1){
+    let sol = "";
+    while (wert1 > 0){
+        if (wert1 & 1){
+            sol = "1" + sol;
+        }else{
+            sol = "0" + sol;
+        }
+    wert1 = wert1 >> 1;
+    }
+    return sol;
+}
+
 function validate(){
-    let correct_answer = eval(input1.value)
-    let user_value = parseFloat(userInput.value)
+    let correct_answer = decitobin(eval(input1.value))
+    let user_value = parseFloat(userInput.value) 
 
-    console.log(correct_answer, user_value);
 
-    if(user_value == correct_answer){
+    if(correct_answer == user_value){
         display()
         user_score++;
         score.innerHTML = `Score: <br> ${user_score}`
@@ -48,10 +65,10 @@ function validate(){
 }
 function runInterval(){
     let timerInterval = setInterval(() => {
-        count_down -= 100;
+        count_down -= 1;
         count_down_element.innerHTML = count_down;
 
-        if(count_down == -100) {
+        if(count_down == -1) {
             alert(`gameOver!, your Highscore: ${user_score}`)
             location.reload()
         }
