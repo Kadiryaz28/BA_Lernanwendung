46fe65d2dde9447ce791bb41a047da1b6b430c81
Kadir <Kadir_yazici28@hotmail.de>
2023-01-06 01:10:37 +0100

    Quellen-Comments eingefügt

--- Bachelor/Grundgerüst/Grundgerüst.js
+++ Bachelor/Grundgerüst/Grundgerüst.js
@@ -3,12 +3,12 @@ const input = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 let images = document.querySelectorAll(".container .image-container .image");
 
- /*Dieser Code zeigt die Funktion für eine Such- und Filterfunktion. 
-  *wenn der User die Suchfunktion anklickt, klappt sie auf und zeigt das Inputfeld. 
-  *Wenn eine Eingabe des Users erfolgt, wird die "images"-Liste gefiltet je nach Wert bei der Eingabe, 
-  *entspricht der Wert der Eingabe des Users dem "game-type"-Attribut des image, wird das image angezeigt. 
-  *Solange die Eingabe leer ist, werden alle images angezeigt.
-  */
+/*Dieser Code zeigt die Funktion für eine Such- und Filterfunktion.
+ *wenn der User die Suchfunktion anklickt, klappt sie auf und zeigt das Inputfeld.
+ *Wenn eine Eingabe des Users erfolgt, wird die "images"-Liste gefiltet je nach Wert bei der Eingabe,
+ *entspricht der Wert der Eingabe des Users dem "game-type"-Attribut des image, wird das image angezeigt.
+ *Solange die Eingabe leer ist, werden alle images angezeigt.
+ */
 const expandSearch = () => {
   searchButton.classList.toggle("close");
   input.classList.toggle("square");
@@ -28,19 +28,19 @@ input.oninput = () => {
   });
 };
 
-// DOM-Elemente, auf die zugegriffen wird 
+// DOM-Elemente, auf die zugegriffen wird
 const buttonItem = document.querySelectorAll(".button-stufe");
 const imageItem = document.getElementsByClassName("gameItem");
 
- /*
-  *Dieser Code beschreibt die Funktion für die Filterboxen, wenn der User auf einen Button klickt, passiert folgendes: 
-  *1. Der "class name" jedes Elements im "buttonItem"-Array wird auf empty string gesetzt 
-  *2. Der "class name" des Buttons, auf den geklickt wurde, wird auf "active" gesetzt. 
-  *3. Mit der Schleife wird durch jedes Element im "imageItem"-Array iteriert. 
-  *4. Wenn der "textContent" "Alle Spiele" ist oder der Wert des aktuellen Elements kleingeschrieben mit dem "data-filter" übereinstimmt,
-  *wird das aktuelle Element in der Schleife angezeigt bzw. nicht angezeigt, wenn es nicht mit "data-filter" übereinstimmt.
-  */
- 
+/*
+ *Dieser Code beschreibt die Funktion für die Filterboxen, wenn der User auf einen Button klickt, passiert folgendes:
+ *1. Der "class name" jedes Elements im "buttonItem"-Array wird auf empty string gesetzt
+ *2. Der "class name" des Buttons, auf den geklickt wurde, wird auf "active" gesetzt.
+ *3. Mit der Schleife wird durch jedes Element im "imageItem"-Array iteriert.
+ *4. Wenn der "textContent" "Alle Spiele" ist oder der Wert des aktuellen Elements kleingeschrieben mit dem "data-filter" übereinstimmt,
+ *wird das aktuelle Element in der Schleife angezeigt bzw. nicht angezeigt, wenn es nicht mit "data-filter" übereinstimmt.
+ */
+
 buttonItem.forEach((button) => {
   button.onclick = function () {
     buttonItem.forEach((button) => {
